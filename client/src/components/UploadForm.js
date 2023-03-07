1a4a43e273b852d8573d7707eb9979061dc09570
Wan Sim <0420syj@naver.com>
2023-03-08 04:20:18 +0900

    [feature] Set uploaded file

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState('이미지 파일을 선택하세요.');
     return (
         <form>
-            <label htmlFor="image">사진</label>
-            <input id="image" type="file" />
+            <label htmlFor="image">{fileName}</label>
+            <input id="image" type="file" onChange={(event) => {
+                setFile(event.target.files[0]);
+                setFileName(event.target.files[0].name);
+            }} />
             <button type="submit">제출</button>
         </form>
     );
