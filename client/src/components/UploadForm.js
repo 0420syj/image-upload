bb47c4b5b3f619fa6a4ef0094ef27dde95be5896
Wan Sim <0420syj@naver.com>
2023-03-08 04:31:50 +0900

    [feature] Upload image from frontend
    Don't forget to add proxy information in package.json

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -3,13 +3,33 @@ import React, { useState } from 'react';
 const UploadForm = () => {
     const [file, setFile] = useState(null);
     const [fileName, setFileName] = useState('이미지 파일을 선택하세요.');
+
+    const imageSelectHandler = (event) => {
+        setFile(event.target.files[0]);
+        setFileName(event.target.files[0].name);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        formData.append('image', file);
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData,
+        }).then((response) => response.json())
+            .then((response) => {
+                console.log(response);
+            });
+    };
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <label htmlFor="image">{fileName}</label>
-            <input id="image" type="file" onChange={(event) => {
-                setFile(event.target.files[0]);
-                setFileName(event.target.files[0].name);
-            }} />
+            <input id="image" type="file" onChange={(event) =>
+                imageSelectHandler(event)
+            } />
             <button type="submit">제출</button>
         </form>
     );
